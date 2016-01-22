546489d0cf379234d6e50b9207594d0e11f06a46
Oscar Niebla Fuentes <oscarniebla@me.com>
2016-01-21 16:06:18 -0800

    Second project of iOS course

--- KCvsSTL/ViewController.swift
+++ KCvsSTL/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var bg: UIImageView!
+    
+    
+    @IBOutlet weak var stlbg: UIImageView!
+    
+    
+    @IBOutlet weak var kcbg: UIImageView!
+    
+    
+    @IBOutlet weak var REDBUTTON: UIButton!
+    
+    
+    @IBOutlet weak var BLUEBUTTON: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +43,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    @IBAction func KCbest(sender: AnyObject) {
+        bg.hidden = true
+        stlbg.hidden = true
+        kcbg.hidden = false
+        REDBUTTON.hidden = true
+        BLUEBUTTON.hidden = false
+    
+    }
+    
+    
+
+    @IBAction func STLBest(sender: AnyObject) {
+        bg.hidden = true
+        stlbg.hidden = false
+        kcbg.hidden = true
+        REDBUTTON.hidden = false
+        BLUEBUTTON.hidden = true
+    }
+
+
+
+
+
 
 
 }
